02b9fab563add46f2ca29fa2e1bade2261305309
1cefr09 <wangxrmail@163.com>
2024-07-21 20:21:04 +0800

    Project Struct

---
+++ FEDA-server/src/test/java/com/example/fedaserver/FedaServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.fedaserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FedaServerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
