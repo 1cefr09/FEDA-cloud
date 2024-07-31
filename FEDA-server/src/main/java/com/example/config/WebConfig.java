850d9f83d30fce0e001dbc16452cd3b0124f57e3
Yoka <a1009216181@gmail.com>
2024-07-31 11:15:16 +0800

    test

--- FEDA-server/src/main/java/com/example/config/WebConfig.java
+++ FEDA-server/src/main/java/com/example/config/WebConfig.java
@@ -17,6 +17,7 @@ public class WebConfig implements WebMvcConfigurer {
     @Autowired
     private JwtTokenAdminInterceptor jwtTokenAdminInterceptor;
 
+
     public void addInterceptors(InterceptorRegistry registry) {
         log.info("开始注册自定义拦截器...");
         registry.addInterceptor(jwtTokenAdminInterceptor)
