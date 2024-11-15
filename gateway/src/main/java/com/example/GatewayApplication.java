96cc478028d706365f4375697d6e515bec6a1a34
1cefr09 <wangxrmail@163.com>
2024-11-15 16:27:25 +0800

    去除了ElasticSearch相关依赖和未实现的功能,开放了服务器的9948和9949端口，gateway服务可以注册到nacos了

---
+++ gateway/src/main/java/com/example/GatewayApplication.java
@@ -0,0 +1,22 @@
+package com.example;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+//import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.scheduling.annotation.EnableAsync;
+import org.springframework.scheduling.annotation.EnableScheduling;
+import org.springframework.transaction.annotation.EnableTransactionManagement;
+
+
+@SpringBootApplication
+@EnableTransactionManagement //开启注解方式的事务管理
+@Slf4j
+@EnableAsync
+@EnableScheduling
+//@EnableDiscoveryClient
+public class GatewayApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(GatewayApplication.class, args);
+        log.info("gateway started");
+    }
+}
